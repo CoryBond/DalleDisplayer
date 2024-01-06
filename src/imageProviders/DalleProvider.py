e7bf13dbc07c07f0898643c84aba9b0df4df1092
CoryBond <cb.cobond@gmail.com>
2024-01-06 23:44:11 +0000

    Added logging

--- src/imageProviders/DalleProvider.py
+++ src/imageProviders/DalleProvider.py
@@ -5,18 +5,25 @@ import requests
 from imageProviders.ImageProvider import ImageProvider
 from PIL import Image
 
+from utils.loggingUtils import generate_logger
+
 
 class DalleProvider(ImageProvider):
+
 
     # inherits from Provider
     def __init__(self, key=None):
         super().__init__(key=key, keyname=key)
 
+        self.logger = generate_logger(self.__class__.__name__)
+
         self.openAiClient = openai.OpenAI(api_key=key)
 
         return
 
+
     def get_image_from_string(self, text, height=0, width=0) -> Image:
+        self.logger.info("Generating image for text : " + text)
         try:
             # Select appropriate size from options in
             # res = list(DalleConst.SIZES.value.keys())[0]
@@ -29,20 +36,20 @@ class DalleProvider(ImageProvider):
 
             response = self.openAiClient.images.generate(
                 model="dall-e-3",
-                prompt="an alien playing hopscotch",
+                prompt="A cool turtle dude",
                 size="1024x1024",
                 quality="standard",
                 n=1,
             )
 
             url = response.data[0].url
-            logging.info("Generated image at : " + url)
+            self.logger.info("Generated image at : " + url)
             img = Image.open(BytesIO(requests.get(url).content))
 
         except BaseException as e:
-            logging.error(e)
+            self.logger.error(e)
             return None
         
-        img.save("test.png", "PNG");
+        # img.save("test.png", "PNG");
 
         return img
